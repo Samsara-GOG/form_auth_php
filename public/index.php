99e42129edc7e9e9a201c77b2d47c1ec1692b629
Samsara <samsaraonaya@gmail.com>
2021-01-04 10:28:34 +0100

    Ajout des fichiers manquants pour les tests de renvois

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "php-intermediate-p7";
+echo "php-intermediate-p7";
+echo "php-intermediate-p7";
+
+echo "php-intermediate-p7";
+echo "php-intermediate-p7";
+echo "php-intermediate-p7";
+
+echo "php-intermediate-p7";
